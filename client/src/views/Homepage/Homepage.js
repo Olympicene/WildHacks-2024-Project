9c1c0166b3f62db51211410b1609e605e847fe96
SarvaniK <skunap2@uic.edu>
2024-04-06 17:47:10 -0500

    main info card done

--- client/src/views/Homepage/Homepage.js
+++ client/src/views/Homepage/Homepage.js
@@ -1,8 +1,20 @@
-import * as React from 'react'
+import React from 'react';
+import MainInfo from '../../components/Card/MainInfo/maininfo';
+
 const Homepage = () => {
+    const userDetails = {
+        name: "Sarvani",
+        age: 21,
+        university: "UCSD",
+        roommates: 2
+    };
+
     return (
-        <h1>This is Home Page</h1>
-    )
-}
+        <div>
+            <h1>This is Home Page</h1>
+            <MainInfo details={userDetails} />
+        </div>
+    );
+};
 
 export default Homepage;
