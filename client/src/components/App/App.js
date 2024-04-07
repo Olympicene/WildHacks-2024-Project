b7e3c9bfe02b3ec0438a2aabceff5bdbb12c0ee7
Nandana <nandanasheri@gmail.com>
2024-04-06 22:15:12 -0500

    worked on log in page

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -2,10 +2,11 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Landing from "../../views/Landing/Landing";
 import Homepage from "../../views/Homepage/Homepage";
 import Matches from "../../views/Matches/Matches";
-import MyMap from "../../views/Mapbox/Map";
 import WithNav from "./WithNav";
 import WithoutNav from "./WithoutNav";
 import Profile from "../../views/Profile/Profile";
+import LoginPage from "../../views/LoginPage/LoginPage";
+import SignUpPage from "../../views/SignUpPage/SignUpPage";
 
 function App() {
   require('./App.css');
@@ -15,6 +16,8 @@ function App() {
       <Routes>
         <Route element={<WithoutNav />}>
           <Route index element={<Landing />} />
+          <Route path="login" element={<LoginPage />} />
+          <Route path="sign-up" element={<SignUpPage />} />
         </Route>
         <Route element={<WithNav />}>
           <Route path="home" element={<Homepage />} />
