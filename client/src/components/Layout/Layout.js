4844ce2f83a254b2e819e889131889a164790d84
Nandana <nandanasheri@gmail.com>
2024-04-07 01:54:53 -0500

    added layout

--- client/src/components/Layout/Layout.js
+++ client/src/components/Layout/Layout.js
@@ -6,62 +6,71 @@ import Guest from '../Card/Guest/Guest';
 import Likes from '../Card/Likes/Likes';
 
 const Layout = () => {
-    const dummy = {
-        user : {
-            name : "nandana",
-            age : "20",
-            university : "uic",
-            roomates : "2",
+    const data = {
+        "first_name": "Aiden",
+        "email": "aiden@example.com",
+        "password": "aiden@123",
+        "age": 22,
+        "university": "Loyola University Chicago",
+        "likes": ["volleyball", "coffee", "hiking"],
+        "dislikes": ["crowded places", "loud noise", "early mornings"],
+        "traits": "Friendly and outdoorsy",
+        "coordinates": {
+            "longitude": -87.6616,
+            "latitude": 42.0012
         },
-        hobbies : {
-            hobby1: "reading", 
-            hobby2: "crocheting", 
-            hobby3: "lifting"
-        },
-        substances : "no smoking",
-        guests : {
-            freq : 3,
-            num : 4
-        },
-        traits : "extroverted",
-        dealbreakers : "no pets",
-        sleep : "11PM to 7AM",
-        allLikes : {
-            like1 : "takeout",
-            like2 : "dogs",
-            like3 : "music",
-            dislike1 : "dishes",
-            dislike2 : "seafood",
-            dislike3 : "soda"
-        },
-        budget : "$1200"
+        "sleepTime": 10,
+        "wakeUpTime": 6,
+        "dealbreakers": "Dishonesty and arrogance",
+        "hobbies": ["surfing", "reading", "exploring new neighborhoods"],
+        "numberOfGuests": "1",
+        "substances": "Non-smoker",
+        "budget": 1400
     }
+    
     require("./Layout.css");
-    const hobbiesList = Object.values(dummy.hobbies); 
+    const hobbiesList = Object.values(data["hobbies"]); 
+    const user = {
+        name : data["first_name"],
+        age : data["age"],
+        university : data["university"],
+        roomates :  Math.floor(Math.random() * 4)
+    }
+
+    const likes = {
+        like1 : data["likes"][0],
+        like2 : data["likes"][1],
+        like3 : data["likes"][2],
+        dislike1 : data["dislikes"][0],
+        dislike2 : data["dislikes"][1],
+        dislike3 : data["dislikes"][2]
+    }
+
+    const sleep = data["sleepTime"] + " PM " + data["wakeUpTime"] + " AM "
 
     return (
         <div className='layout-container'>
             <div>
-            <a ><img src={require('./ex.png')} class="pic" onClick="moveright()" /></a>
+            <a><img src={require('./ex.png')} class="pic" onClick="moveright()" /></a>
             </div>
 
             <div className='semi-container'>
                 <Info title="hobbies" details={hobbiesList.join(", ")}/>
-                <Guest value={dummy.guests}/>
-                <Info title="substances" details={dummy.substances}/>
+                <Guest value={data["numberOfGuests"]}/>
+                <Info title="substances" details={data["substances"]}/>
             </div>
 
             <div className='semi-container' style={{alignItems:"center"}}>
                 <Photo/>
-                <MainInfo details={dummy.user}/>
-                <Likes details={dummy.allLikes}/>
+                <MainInfo details={user}/>
+                <Likes details={likes}/>
             </div>
 
             <div className='semi-container'>
-                <Info title="traits" details={dummy.traits}/>
-                <Info title="sleep schedule" details={dummy.sleep}/>
-                <Info title="dealbreakers" details={dummy.dealbreakers}/>
-                <Info title="budget" details={dummy.budget}/>
+                <Info title="traits" details={data["traits"]}/>
+                <Info title="sleep schedule" details={sleep}/>
+                <Info title="dealbreakers" details={data["dealbreakers"]}/>
+                <Info title="budget" details={data["budget"]}/>
             </div>
 
             <div>
